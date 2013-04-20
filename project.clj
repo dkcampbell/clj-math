6885e1d25bb16945b7ac3d3c904add7978507e77
Dan Campbell <dan.campbell@gmail.com>
2013-04-20 13:41:42 -0500

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject clj-math "1.0.0-SNAPSHOT"
+  :description "Collection of basic math functions."
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/clj_math/test/core.clj
@@ -0,0 +1,6 @@
+(ns clj-math.test.core
+  (:use [clj-math.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
