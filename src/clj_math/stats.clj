5fd92403d8c93b241760673976eae1ea2281e034
Dan Campbell <dan.campbell@gmail.com>
2013-04-25 22:57:45 -0500

    Add function for calculating the median.

--- src/clj_math/stats.clj
+++ src/clj_math/stats.clj
@@ -2,4 +2,18 @@
 
 ; Scale a number between 0 and 1.
 (defn scale [n min max] 
-	(/ n (- max min)))
+  (/ n (- max min)))
+
+; Calculate the mean of a list.
+(defn mean [xs]
+  (/ (apply + xs) (count xs)))
+
+; Calculate the median of a list.
+; Naieve implementation
+(defn median [xs]
+  (let [ordered-list (sort xs)
+        length (count ordered-list)]
+    (if (even? length) 
+      (mean [(nth ordered-list (quot length 2))
+             (nth ordered-list (- (quot length 2) 1))])
+      (nth ordered-list (quot length 2)))))
