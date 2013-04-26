a61873417f1a926acb6ea5abec1cfc90ebb561ca
Dan Campbell <dan.campbell@gmail.com>
2013-04-26 14:35:36 -0500

    Add dependency for clojure-contrib.

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clj-math "1.0.0-SNAPSHOT"
   :description "Collection of basic math functions."
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
