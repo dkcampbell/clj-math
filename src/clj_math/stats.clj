fa7c66f1f30dbdb8bd55b9185a57ec66af58d9dd
Dan Campbell <dan.campbell@gmail.com>
2013-04-29 20:42:52 -0500

    Add doc strings.

--- src/clj_math/stats.clj
+++ src/clj_math/stats.clj
@@ -1,16 +1,22 @@
 (ns clj-math.stats [:require [clojure.contrib [math :as math]]])
 
 ; Scale a number between 0 and 1.
-(defn scale [n min max] 
+(defn scale 
+  "Scale a number between 0 and 1."
+  [n min max] 
   (/ n (- max min)))
 
 ; Calculate the mean of a list.
-(defn mean [xs]
+(defn mean 
+  "Calculate the arithmetic mean of a collection."
+  [xs]
   (/ (apply + xs) (count xs)))
 
 ; Calculate the median of a list.
 ; Naieve implementation
-(defn median [xs]
+(defn median 
+  "Calculate the median of a list."
+  [xs]
   (let [ordered-list (sort xs)
         length (count ordered-list)]
     (if (even? length) 
@@ -21,19 +27,25 @@
 ; Find the most common element in a list.
 ; If more than one number occurs equally as often. The first number in the 
 ; list will be returned.
-(defn mode [xs]
+(defn mode 
+  "Find the most common element in a list."
+  [xs]
   (let [tally (frequencies xs)]
     (ffirst (into 
           (sorted-map-by #(compare (get tally %2) (get tally %1))) tally))))
 
 ; Calculate variance
-(defn variance [xs]
+(defn variance 
+  "Calculate the variance of a list."
+  [xs]
   (let [m (mean xs)
         length (count xs)]
     (/ (apply + (map #(math/expt (- %1 m) 2) xs)) (- length 1))))
 
 ; Standard Deviation
-(defn standard-deviation [xs]
+(defn standard-deviation 
+  "Calculate standard deviation of a list."
+  [xs]
   (math/sqrt (variance xs)))
 
 ; Quartiles
