0c3f92e5cd015c087691ce1346983af59ff4ca62
Dan Campbell <dan.campbell@gmail.com>
2013-05-08 21:09:31 -0500

    Add a few more basic test.

--- test/clj_math/test/stats.clj
+++ test/clj_math/test/stats.clj
@@ -4,4 +4,7 @@
 
 (deftest med 
   (is (= (median [1 2 3]) 2))
-  (is (= (median [1 2 3 4]) 5/2)))
+  (is (= (median [1 2 3 4]) 5/2))
+  (is (= (median [0 0 0 0]) 0))
+  (is (= (median [4 3 2 1]) 5/2))
+
